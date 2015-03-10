02086ffa46ef1d7c86d45433e1d08eb2f511f031
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-03-10 12:00:49 +0100

    initial commit

---
+++ DesignOptionModifier/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DesignOptionModifier
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button_ok = new System.Windows.Forms.Button();
+			this.buttonGet = new System.Windows.Forms.Button();
+			this.buttonSet = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.buttonSet);
+			this.groupBox1.Controls.Add(this.buttonGet);
+			this.groupBox1.Controls.Add(this.listBox1);
+			this.groupBox1.Location = new System.Drawing.Point(13, 13);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(448, 372);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "DesignOptions";
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.IntegralHeight = false;
+			this.listBox1.Location = new System.Drawing.Point(7, 20);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(435, 317);
+			this.listBox1.TabIndex = 0;
+			// 
+			// button_ok
+			// 
+			this.button_ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button_ok.Location = new System.Drawing.Point(387, 394);
+			this.button_ok.Name = "button_ok";
+			this.button_ok.Size = new System.Drawing.Size(75, 23);
+			this.button_ok.TabIndex = 1;
+			this.button_ok.Text = "OK";
+			this.button_ok.UseVisualStyleBackColor = true;
+			this.button_ok.Click += new System.EventHandler(this.button_ok_Click);
+			// 
+			// buttonGet
+			// 
+			this.buttonGet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.buttonGet.Location = new System.Drawing.Point(7, 343);
+			this.buttonGet.Name = "buttonGet";
+			this.buttonGet.Size = new System.Drawing.Size(75, 23);
+			this.buttonGet.TabIndex = 1;
+			this.buttonGet.Text = "Get";
+			this.buttonGet.UseVisualStyleBackColor = true;
+			this.buttonGet.Click += new System.EventHandler(this.buttonGet_Click);
+			// 
+			// buttonSet
+			// 
+			this.buttonSet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.buttonSet.Location = new System.Drawing.Point(88, 343);
+			this.buttonSet.Name = "buttonSet";
+			this.buttonSet.Size = new System.Drawing.Size(75, 23);
+			this.buttonSet.TabIndex = 2;
+			this.buttonSet.Text = "Set";
+			this.buttonSet.UseVisualStyleBackColor = true;
+			this.buttonSet.Click += new System.EventHandler(this.buttonSet_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(474, 429);
+			this.Controls.Add(this.button_ok);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button buttonSet;
+		private System.Windows.Forms.Button buttonGet;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button_ok;
+	}
+}
+
